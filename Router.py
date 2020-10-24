6ceb36f8f26bdb5e7bc60c20bc0af502702e38d0
Neuro <Lorenzocurcio2@gmail.com>
2020-10-25 00:57:55 +0100

    Lots of comments

--- Router.py
+++ Router.py
@@ -1,15 +1,13 @@
 import math
 import json
-import copy
 from networkx import *
 
-AU=149597870700
+AU = 149597870700
 jsonData = open("stargateDB.json").read()
 data = json.loads(jsonData)
 
 
 def warpTime(warpSpeed, subSpeed, warpDist):
-
     warpDist = warpDist * AU
     k_accel = warpSpeed
     k_decel = min(warpSpeed / 3, 2)
@@ -32,59 +30,27 @@ def warpTime(warpSpeed, subSpeed, warpDist):
     accel_time = math.log(max_ms_warp_speed / k_accel) / k_accel
     decel_time = math.log(max_ms_warp_speed / warp_dropout_speed) / k_decel
 
-
     total_time = cruise_time + accel_time + decel_time
     return total_time
 
+
 def mapMaker(map, warpSpeed, subSpeed, align):
     for edge in map.edges(data=True):
-        if edge[2]["type"] == 0:
+        if edge[2]["type"] == 0:  # 0 is an inter-stargate edge representing align times
             edge[2]["weight"] = align
-        if edge[2]["type"] == 1:
-            edge[2]["weight"] = warpTime(warpSpeed,subSpeed,edge[2]["weight"])
+        if edge[2]["type"] == 1:  # 1 is an intra-stargate edge representing warp times, so the warp time is calculated using edge weight
+            edge[2]["weight"] = warpTime(warpSpeed, subSpeed, edge[2]["weight"])
     return map
 
-def routingPath(map,start,end):
-    map.add_node("Start",system=111)
-    map.add_node("End",system=999)
+
+def routingPath(map, start, end):
+    map.add_node("Start", system=111)  # adds fake start node
+    map.add_node("End", system=999)  # adds fake end node
     for node in map.nodes(data=True):
-        if node[1]["system"] == str(start):
-            map.add_edge("Start",node[0])
+        if node[1]["system"] == str(start):  # links stargate nodes to start node weight zero
+            map.add_edge("Start", node[0])
             print("Start Linked")
-        elif node[1]["system"] == str(end):
-            map.add_edge("End",node[0])
+        elif node[1]["system"] == str(end):  # links stargate nodes to end node weight zero
+            map.add_edge("End", node[0])
             print("End Linked")
-    print("Modified")
-    return dijkstra_path(map,"Start","End")
-
-def routingLength(map,start, end):
-    map.add_node("Start",system=111)
-    map.add_node("End",system=999)
-    for node in map.nodes(data=True):
-        if node[1]["system"] == str(start):
-            map.add_edge("Start",node[0])
-        elif node[1]["system"] == str(end):
-            map.add_edge("End",node[0])
-    length = dijkstra_path_length(map,"Start","End")
-    map.remove_node("Start")
-    map.remove_node("End")
-    return length
-
-def center(map):
-    out = {}
-    counter = 0
-    for start in data.keys():
-        out[start] = 0
-        print(start)
-        print(out)
-        for end in data.keys():
-            if end != start:
-                counter = counter + 1
-                print(counter)
-                if counter == 1000:
-                    return out
-                else:
-                    out[start] = out[start] + routingLength(map,start,end)
-    return out
-
-read_yaml("Atron.yaml")
+    return dijkstra_path(map, "Start", "End")  # calculates shortest path
--- retRouter.py
+++ retRouter.py
@@ -3,98 +3,104 @@ import json
 import pickle
 from retworkx import *
 
-AU = 149597870700
-jsonData = open("stargateDB.json").read()
+AU = 149597870700  # Astronomical Unit in meters
+jsonData = open("stargateDB.json").read()  # used for system and stargate data
 data = json.loads(jsonData)
-jsonData = open("Mapping.json").read()
+jsonData = open("Mapping.json").read()  # used to map stargate IDs to internal node IDs
 mapping = json.loads(jsonData)
-jsonData = open("reverseMapping.json").read()
+jsonData = open("reverseMapping.json").read()  # the mapping of node IDs to the system their stargate is in
 reverseMapping = json.loads(jsonData)
 
-warpSpeedGL = 0
-subSpeedGL = 0
-alignGL = 0
+warpSpeedGL = 0  # Global variables for Warp Speed, Sublight Speed, and Align time.
+subSpeedGL = 0  # Used to feed the EdgeWeight function
+alignGL = 0  # Unfortunately required as the function is automatically used by the package and can only have one input
 
-def writer(target, tbw):
+
+def writer(target, tbw):  # basic utility function to write Json
     with open(target, 'w') as outfile:
         json.dump(tbw, outfile)
 
 
-def warpTime(warpSpeed, subSpeed, warpDist):
-    warpDist = warpDist * AU
+def warpTime(warpSpeed, subSpeed, warpDist):  # Calculates warp time
+    warpDist = warpDist * AU  # Calculates warp distance in m by multiplying input in AU by constant of m in AU
     k_accel = warpSpeed
-    k_decel = min(warpSpeed / 3, 2)
+    k_decel = min(warpSpeed / 3, 2)  # warp deceleration is usually half of the warp speed but is also capped at 3AU/s^2
 
-    warp_dropout_speed = min(subSpeed / 2, 100)
-    max_ms_warp_speed = warpSpeed * AU
+    warp_dropout_speed = min(subSpeed / 2, 100)  # Likewise warp dropout speed is half of sublight speed or capped at 100m/s
+    max_ms_warp_speed = warpSpeed * AU  # maximum warp speed in m/s, calculated bu multiplying by constant of m in AU
 
-    accel_dist = AU
-    decel_dist = max_ms_warp_speed / k_decel
+    accel_dist = AU  # The way warp acceleration works in EVE is that one always travels exactly one AU while accelerating from 0 to max warp speed
+    decel_dist = max_ms_warp_speed / k_decel  # how long distance wise will be spent decelerating
 
-    minimum_dist = accel_dist + decel_dist
+    minimum_dist = accel_dist + decel_dist  # the minimum warp distance for there to be a period of travel at maximum speed instead of beginning to
+    # decelerate before reaching it
 
     cruise_time = 0
 
-    if minimum_dist > warpDist:
-        max_ms_warp_speed = warpDist * k_accel * k_decel / (k_accel + k_decel)
+    if minimum_dist > warpDist:  # if there will not be a period of maximum speed travel,
+        max_ms_warp_speed = warpDist * k_accel * k_decel / (k_accel + k_decel)  # top speed reached in warp if not maximum
     else:
-        cruise_time = (warpDist - minimum_dist) / max_ms_warp_speed
+        cruise_time = (warpDist - minimum_dist) / max_ms_warp_speed  # if there will be a period of maximum speed travel how long will it take
 
-    accel_time = math.log(max_ms_warp_speed / k_accel) / k_accel
-    decel_time = math.log(max_ms_warp_speed / warp_dropout_speed) / k_decel
+    accel_time = math.log(max_ms_warp_speed / k_accel) / k_accel  # how long to accelerate to top speed reached during warp, which is not
+    # necessarily maximum warp speed
+    decel_time = math.log(max_ms_warp_speed / warp_dropout_speed) / k_decel  # how long to decelerate from top speed reached in warp to warp
+    # dropout speed
 
     total_time = cruise_time + accel_time + decel_time
     return total_time
 
 
-def mapgen(map):
+def mapgen(map):  # Map loader. Depickles a weighted edge list and builds a graph from it
     with open(map, 'rb') as edges:
         edgeList = pickle.load(edges)
     retMap = PyGraph()
     retMap.extend_from_weighted_edge_list(edgeList)
-    for node in retMap.node_indexes():
+    for node in retMap.node_indexes():  # adds stargate IDs to the nodes as data using a precompiled JSON list
         retMap[node] = reverseMapping[str(node)]
     return retMap
 
 
 def edgeWeight(edge):
-    if edge["type"] == 0:
+    if edge["type"] == 0:  # 0 is an inter-stargate edge representing align times
         return alignGL
-    elif edge["type"] == 1:
-        return warpTime(warpSpeedGL,subSpeedGL,edge["weight"])
-    elif edge["type"] == 2:
+    elif edge["type"] == 1:  # 1 is an intra-stargate edge representing warp times, so the warp time is calculated using edge weight
+        return warpTime(warpSpeedGL, subSpeedGL, edge["weight"])
+    elif edge["type"] == 2:  # 2 is a special edge used to connect "system" start nodes to system stargates; it is always 0
         return edge["weight"]
 
 
 def routingLength(map, start):
-    startID = map.add_node(111)
+    startID = map.add_node(111)  # creates a node used to simulate starting in a system; because starting at any one stargate would be undue bias
     edgeData = {"type": 2, "weight": 0}
-    for key, val in reverseMapping.items():
+    for key, val in reverseMapping.items():  # cycles through all stargate nodes linking those that belong to the start system to it
         if val == str(start):
             map.add_edge(startID, int(key), edgeData)
-    length = graph_dijkstra_shortest_path_lengths(map, startID, edgeWeight)
-    map.remove_node(startID)
+    length = graph_dijkstra_shortest_path_lengths(map, startID, edgeWeight)  # Inbuilt function which calculates all shortest paths from a node
+    map.remove_node(startID)  # removes temporary system node
     return length
 
 
-def center(map,warp_speed,sub_speed,align_speed):
-    map = mapgen(map)
-    global alignGL
+def center(map, warp_speed, sub_speed, align_speed):
+    map = mapgen(map)  # loads map
+    global alignGL  # calling global scope
     global warpSpeedGL
     global subSpeedGL
-    alignGL = align_speed
+    alignGL = align_speed  # setting globals
     warpSpeedGL = warp_speed
     subSpeedGL = sub_speed
     out = {}
     counter = 0
-    for start in data.keys():
+    for start in data.keys():  # iterates through all systems
         out[start] = {}
         total = 0
         counter = counter + 1
-        if counter % 10 == 0:
+        if counter % 10 == 0:  # simple counter to visualize progress
             print(counter)
-        inter = routingLength(map,start)
-        for end,gates in data.items():
+        inter = routingLength(map, start)  # gets shortest paths for current system
+        for end, gates in data.items(): # Since nodes in this map are stargates not systems, they need to be compacted down into system-system
+            # distances instead of system-stargate distances, this is done by iterating through the results and using only the shortest distance
+            # for each system as the system-system length
             min = 0
             if end != start:
                 for gate in gates["stargates"]:
@@ -109,4 +115,4 @@ def center(map,warp_speed,sub_speed,align_speed):
     return out
 
 
-writer("center.json",center("retMap",8.69,656,2))
+writer("center.json", center("retMap", 8.69, 656, 2))
